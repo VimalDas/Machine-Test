3c0425ec41e1b2980bc472f05b3969b1d88ee4f5
Vimal Das <vimaldasu@gmail.com>
2021-04-09 14:08:31 +0530

    Initial Commit

---
+++ MachineTest_vimal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineTest_vimal
+//
+//  Created by Vimal Das on 09/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
